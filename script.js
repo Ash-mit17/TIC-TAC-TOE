831a4ff3b54a3e636648713be25c39746b6af38f
Ashmit Ranjan <121622487+Ash-mit17@users.noreply.github.com>
2023-03-28 16:58:40 +0530

    update

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ if(i++%2!=0)
     if(flag==0){
     $(".win").text("Player "+Number(turn)+" turn")
     turn=turn==1?2:1;}
-    if(i==10)
+    if(i==10 && flag!=1)
     {$(".win").text("Match Drawn");
     location.reload()}
 }
@@ -79,7 +79,7 @@ else
     if(flag==0){
     $(".win").text("Player "+Number(turn)+" turn")
     turn=turn==1?2:1;}
-    if(i==11)
+    if(i==10 && flag!=1)
     {$(".win").text("Match Drawn")
 
     setTimeout(function (){location.reload()},1000)
